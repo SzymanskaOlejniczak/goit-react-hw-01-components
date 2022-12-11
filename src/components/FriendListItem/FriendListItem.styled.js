723bfe3fa7b2e50d31aada4961cfd7e2709f977d
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2022-12-12 00:21:05 +0100

    Update FriendListItem.styled.js

--- src/components/FriendListItem/FriendListItem.styled.js
+++ src/components/FriendListItem/FriendListItem.styled.js
@@ -5,7 +5,7 @@ export const Status = styled.span`
   width: 20px;
   height: 20px;
   border-radius: 50%;
-  margin-right: 10px;
+  margin-right: 0 auto;
   background-color: ${props => (props.statusType ? 'green' : 'red')};
 `;
 export const Name = styled.p`
