49d0b63f390bd1a77b21da109043ee3eb32139c7
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2022-12-12 00:14:24 +0100

    Create FriendList.styled.js

---
+++ src/components/FriendList/FriendList.styled.js
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const FriendListUl = styled.ul`
+  display: flex;
+  width: 400px;
+  flex-direction: column;
+  margin: 0 auto;
+  list-style-type: none;
+  padding: 0;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  background-color: rgb(164, 205, 152);
+  border: 2px solid #fff;
+`;
+export const Item = styled.li`
+  display: flex;
+  margin-bottom: 30px;
+  align-items: center;
+  justify-content: center;
+`;
