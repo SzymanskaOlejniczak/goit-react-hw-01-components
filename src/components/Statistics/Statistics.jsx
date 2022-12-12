c2053ef194be05f87d9e1577c763e5cc5e119a2d
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2022-12-13 00:26:09 +0100

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,25 +1,25 @@
 import PropTypes from 'prop-types';
-import { Title, SectionStat, StatList, Item } from './Statistics.styled';
+import styles from './Statistics.module.css';
 export function Statistics ({title ='Upload stats', stats}){
     function randomHexColor() {
         return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
       }
     return (
-        <SectionStat>
-            {title && <Title>{title}</Title>}
-            <StatList>
+        <section className={styles.sectionStat}>
+            {title && <h2 className={styles.title}>{title}</h2>}
+            <ul className={styles.statList}>
         {stats.map(({ id, label, percentage }) => (
-                <Item
-                className="item"
+                <li
+                className={styles.item}
                 key={id}
                 style={{ backgroundColor: randomHexColor() }}
                 >
                 <span className="label">{label}</span>
             <   span className="percentage">{percentage}%</span>
-                </Item>
+                </li>
         ))}
-            </StatList>
-        </SectionStat>
+            </ul>
+        </section>
     );
 }
 Statistics.propTypes = {
