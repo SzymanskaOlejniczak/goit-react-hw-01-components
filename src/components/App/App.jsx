fb886a4285c8863f674cbf145010ac71f36db4e3
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2022-12-14 21:50:52 +0100

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,7 @@ import data from '../../data/data.json';
 import friends from '../../data/friends.json';
 import transactions from '../../data/transactions.json';
 import Statistics from '../Statistics/Statistics';
-import Profile from '../Profile/Profile1';
+import Profile from '../Profile/Profile';
 import styles from './App.module.css';
 import FriendList from '../FriendList/FriendList';
 import TransactionHistory from '../TransactionHistory/TransactionHistory';
